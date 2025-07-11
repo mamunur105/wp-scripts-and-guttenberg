406db94f1717592191a03f0b3df6a7131e878d72
Mamunur Rashid <30565480+mamunur105@users.noreply.github.com>
2025-07-11 09:56:27 +0600

    .

--- build/blocks/add-to-cart/render.php
+++ build/blocks/add-to-cart/render.php
@@ -1,10 +1,19 @@
 <?php
-if ( !  function_exists( 'atttrrrrrr' ) ) {
+/**
+ * Template variables:
+ *
+ * @var $attributes                    atrributes.
+ */
+if ( ! function_exists( 'atttrrrrrr' ) ) {
 	function atttrrrrrr( $attributes ) {
 		if ( empty( $attributes['productId'] ) ) {
 			return '<div style="color:red;">No product selected.</div>';
 		}
-		return '<div style="color:green;">Product ID: ' . intval( $attributes['productId'] ) . '</div>';
+        global $product;
+        $product = wc_get_product( $attributes['productId'] );
+		ob_start();
+		woocommerce_template_single_add_to_cart();
+		return ob_get_clean();
 	}
 }
 echo atttrrrrrr( $attributes );
